84858a06955d56385441fdc2d74c810fcb614412
mrmaiyani <mrmaiyani@gmail.com>
2026-03-17 00:41:59 +0530

    Add files via upload

---
+++ Python/set_method.py
@@ -0,0 +1,19 @@
+items = {"apple","banana","tomato","brinjal"}
+
+# items.add("cherry")
+# print(items)
+
+# items.update(["mango","peach"])
+# print(items)
+
+# print(items)
+# items.remove("bananae") #error
+# items.discard("bananae")  #no error
+
+# a = items.pop()
+# print(items)
+# print(a)
+
+print(len(items))
+items.clear()
+print(items)
