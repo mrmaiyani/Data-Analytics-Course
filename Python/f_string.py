857013e3313cd6a21b49def6397173203d74abc4
mrmaiyani <mrmaiyani@gmail.com>
2026-03-17 00:59:57 +0530

    Add files via upload

---
+++ Python/f_string.py
@@ -0,0 +1,9 @@
+name = input("Enter your Name :")
+work = input("Where Do You Study? :")
+live = input("Where Do You Live? :")
+
+# Without f string
+# print(name + " WOrks At " + work +" and lives in " + live)
+
+# with f String
+print(f"{name} works at {work} and lives in {live}") 
