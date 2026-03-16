25d7e59b6f95c778fc7e34fb41657d66a0dd9d2a
mrmaiyani <mrmaiyani@gmail.com>
2026-03-17 00:49:28 +0530

    Add files via upload

---
+++ Python/if_else_condition.py
@@ -0,0 +1,13 @@
+score = 85
+
+if score >= 80:
+    print("Grade A")
+elif score >= 60:
+    print("Grade B")
+else:
+    print("Grade C")
+
+if score % 2 == 0:
+    print("score is even!")
+else:
+    print("score is odd!")
