2386f30e77f59b0b4884789a1a154b470feddd41
mrmaiyani <mrmaiyani@gmail.com>
2026-03-17 00:38:11 +0530

    Add files via upload

---
+++ Python/tuple_method.py
@@ -0,0 +1,14 @@
+items = ("apple","banana","orange")
+
+print(items[-1])
+print(len(items))
+print(items.count("apple"))
+print(items[0:2])
+
+print(items.index("banana"))
+
+list_1 = list(items)
+print(list_1)
+
+for item in items:
+    print(item)
