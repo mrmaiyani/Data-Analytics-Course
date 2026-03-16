91ea4b1cbcac4ab4cac405a3bed051f5ab016a2e
mrmaiyani <mrmaiyani@gmail.com>
2026-03-17 00:37:15 +0530

    Add files via upload

---
+++ Python/list.py
@@ -0,0 +1,14 @@
+names = ["Utsav","Krishna","Radhe","Govind","Hari"]
+elements = [1, 34, 67, False, True]
+
+print(names)
+print(type(names))
+print(elements[0])
+print(elements[-1])
+
+print(elements[1:4])
+print(len(elements))
+
+# Lists in python are mutable
+elements[2] = 69
+print(elements)
