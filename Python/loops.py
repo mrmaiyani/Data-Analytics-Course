9d7825193a0923b9cb6cc2e101a5e7e50e6ca575
mrmaiyani <mrmaiyani@gmail.com>
2026-03-17 00:49:52 +0530

    Add files via upload

---
+++ Python/loops.py
@@ -0,0 +1,18 @@
+items = ["apples", 34, "bananas", "oranges"]
+
+for item in items:
+    print(item)
+
+for i in range(0, 10, 3):
+    print(i)
+
+for char in "Utsav":
+    print(char)
+
+count = 1
+
+while (count <= 5):
+    print(count)
+    count += 1
+
+print("End of Program")
