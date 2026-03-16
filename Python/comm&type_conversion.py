8c9b0182a6412fdae8f30ba46cf5d10d2a0d035d
mrmaiyani <mrmaiyani@gmail.com>
2026-03-17 00:33:46 +0530

    Add files via upload

---
+++ Python/comm&type_conversion.py
@@ -0,0 +1,27 @@
+# comments :
+
+# This will print hello world
+print("Hello World!") #something
+
+''' This
+    is
+    Multiline 
+    comment'''
+
+# Type Conversion :
+
+age = "45"
+
+print(int(age))
+print(int(age) +5)
+
+price = "2222.22"
+print(float(price) +5)
+
+age = 56
+
+print(str(age))
+
+num = "456radhe"
+print(int(num)) # invalid
+
