016d7126a7adfd2d93d664b8c8b30df34a1abd99
mrmaiyani <mrmaiyani@gmail.com>
2026-03-17 00:35:43 +0530

    Add files via upload

---
+++ Python/string.py
@@ -0,0 +1,14 @@
+name = "Utsav"
+
+print(name[0])
+print(name[1])
+print(name[2])
+print(name[3])
+print(name[4])
+# print(name[5]) # give an error
+
+naam_jap = '''radha radha radha radha radha radha radha
+radha radha radha radha radha radha radha radha '''
+
+print(naam_jap)
+print(naam_jap[0])
