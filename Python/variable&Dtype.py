7d1f8b4eb7ff8af25dc79f27e4058a3e6e82b23e
mrmaiyani <mrmaiyani@gmail.com>
2026-03-17 00:32:51 +0530

    Add files via upload

---
+++ Python/variable&Dtype.py
@@ -0,0 +1,16 @@
+a = 22
+
+price = 10.10
+
+name = "utsav"
+city = "Surat"
+
+is_integer = True
+is_float = False
+
+print(type(a))
+print(type(price))
+print(type(city))
+print(type(name))
+print(type(is_integer))
+print(type(is_float))
