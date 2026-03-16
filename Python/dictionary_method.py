887960221942e6e4b4d981c7d02f5990563d0028
mrmaiyani <mrmaiyani@gmail.com>
2026-03-17 00:58:29 +0530

    Add files via upload

---
+++ Python/dictionary_method.py
@@ -0,0 +1,19 @@
+students = {
+    "Name" : "Utsav",
+    "City" : "Surat",
+    "Company" : "Infosys"
+}
+
+# students.pop("Name")
+# print(students)
+
+# students["Class"] = "SYBSCDS"
+# print(students)
+# students.popitem()
+
+# # del students['City']
+# students.clear()
+# print(students.keys())
+# print(students.values())
+print(students.items())
+print(students)
