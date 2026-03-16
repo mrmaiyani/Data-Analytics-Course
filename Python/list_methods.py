d2aa10b08b77bd0ce6f327b52172438f166c2581
mrmaiyani <mrmaiyani@gmail.com>
2026-03-17 00:59:39 +0530

    Add files via upload

---
+++ Python/list_methods.py
@@ -0,0 +1,18 @@
+items = ["apple","banana","orange"]
+# print(len(items))
+
+# items.append("strawberry")
+# items.insert(1,"strawberry")
+# items.extend(["more bananas","pineapples"])
+# items.remove("banana")
+# items.pop(1)
+# items.clear()
+# print(items.index("banana"))
+# print(items.count("banana"))
+print(items)
+
+numbers = [11,22,78,94,6,54,3,9]
+numbers.sort(reverse=True)
+print(numbers)
+
+print(11 in numbers)
