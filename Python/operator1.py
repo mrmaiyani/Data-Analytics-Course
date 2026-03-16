9e4a8f16c5be3875009586c946fc1f7668ede066
mrmaiyani <mrmaiyani@gmail.com>
2026-03-17 00:35:02 +0530

    Add files via upload

---
+++ Python/operator1.py
@@ -0,0 +1,29 @@
+a = 10 # Assignment Operator
+b = 3
+
+# Arithmetic Operator
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+print(a // b)
+print(a % b)
+print(a ** b)
+
+# Comparison Operator
+print(a == b)
+print(a != b)
+print(a > b)
+print(a >= b)
+print(a < b)
+print(a <= b)
+
+a += 4
+print(a)
+
+print(False and True)
+print(True and True)
+print(False or True)
+print(True or True)
+print(not (True))
+print(not(False))
