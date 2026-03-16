431c7ffe0effb05d52640eb2c2cde90efaccf38b
mrmaiyani <mrmaiyani@gmail.com>
2026-03-17 00:53:12 +0530

    Add files via upload

---
+++ Python/function.py
@@ -0,0 +1,14 @@
+def greet(fname, lname):
+    print("Good Morning!", fname, lname)
+    print("How Are You!")
+    print("Thank You!")
+
+greet("Vrunda", "Kakadiya")
+greet("utsav", "Maiyani")
+
+def add(a, b):
+    print(a+b)
+    return a + b
+
+c = add(4, 6)
+print(c)
